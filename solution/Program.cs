7f8ef132f6ac66b57f769ffb744dbc2ced4541e2
Elena <glich1990@gmail.com>
2024-06-24 23:16:47 +0300

    Цикл для определения длины нового массива

--- solution/Program.cs
+++ solution/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] str_array = ["Hello", "2", "world", ":-)"];
+int count = 0;
+
+foreach(string str in str_array)
+{
+    if(str.Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] res_array= new string[count];
+int j = 0;
